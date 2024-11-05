1e8b23f8642c0f10603a505f98c31616419272f2
Lorran Henrique Rezende Alves <logplayviews@gmail.com>
2024-11-05 20:25:17 -0300

    conexão com o BD e tratamento
    -Conexão mais recente com o BD
    -Criação de uma função de reset do banco de dados de teste

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
-const transacaoMiddleware = require('./middleware/middlewareTransacao');
 require('dotenv').config();
 
 const app = express();
@@ -33,7 +32,7 @@ const dbURI = process.env.MONGO_URI; //URI aqui
 let globalSession;
 
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbURI)
     .then(async () => {
         globalSession = await mongoose.startSession();
         globalSession.startTransaction();
@@ -41,7 +40,52 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         app.listen(3000);
     })
     .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
-    
+
+
+const resetDatabase = async () => {
+    console.log("Resetando o banco de dados...");
+
+    try {
+        // Apaga todos os documentos de todas as coleções
+        const collections = await mongoose.connection.db.collections();
+        for (let collection of collections) {
+            await collection.deleteMany({});
+        }
+
+        await Usuario.create({
+            nome: 'admin',
+            senha: "000",
+            cargo: 'Administrador'
+        });
+
+        console.log("Banco de dados resetado e administrador recriado.");
+    } catch (err) {
+        console.error("Erro ao resetar o banco de dados:", err);
+    }
+};
+
+process.on('SIGINT', async () => {
+    try {
+        await resetDatabase();
+        await globalSession.abortTransaction(); // Reverte todas as transações
+        await globalSession.endSession();
+
+        await mongoose.connection.close(); // Remove o callback e apenas espera a Promise
+
+        console.log("Conexão com o MongoDB encerrada.");
+        process.exit(0);
+    } catch (err) {
+        console.error("Erro ao encerrar a conexão com o MongoDB:", err);
+        process.exit(1);
+    }
+});
+
+process.on('uncaughtException', async (err) => {
+    console.error("Exceção não tratada:", err);
+    await resetDatabaseOnExit();
+    process.exit(1);
+});
+
 
 app.use(rotasLogin);
 app.use(rotasAutentificar);
